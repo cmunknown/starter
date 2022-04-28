95453b613c2661ec0c35d009a0d6e5a0bc5264a9
cmunknown <cmunknown@mail.ru>
2022-04-28 12:18:12 +0300

    1.0

---
+++ src/main/java/com/example/starterantontask/StarterAntonTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.starterantontask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterAntonTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterAntonTaskApplication.class, args);
+    }
+
+}
